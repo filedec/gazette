66fd84021a56b24a54624572a75e6b12137896e1
nischaltimalsina <nischaltimalcna@outlook.com>
2024-02-09 14:35:16 +0545

    init: initial deployment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
     <h2 className="text-2xl text-center">Doqett</h2> 
+    <h2 className="text-xl text-center">We will be live soon. Stay tuned! 😊</h2> 
     </main>
   );
 }
